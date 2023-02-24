a84d9ced4895af980e431f4b259ca3ef99b41267
SFERAS <rthyssen73@gmail.com>
2023-02-23 22:14:02 -0300

    atempt to get some staff from DCI

--- src/components/section-picture/Picture.jsx
+++ src/components/section-picture/Picture.jsx
@@ -6,7 +6,7 @@ export default function Pictuer() {
 	return (
 		<div id="foto" className="picture">
 			<div className="picture-container">
-				<img src={home} alt="" />
+				<img src={home} alt="baner" className="onscroll-image-fade-in"/>
 			</div>
 			<div className="moto">
 				<h1>¡AQUÍ PARA AYUDARLE A ENCONTRAR LA CASA DE SUS SUEÑOS!</h1>
